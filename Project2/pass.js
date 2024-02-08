2b67a7ea946fabf81002e275b3bac31caeaa8ff3
prateekbisht23 <143113996+prateekbisht23@users.noreply.github.com>
2024-02-09 00:29:27 +0530

    Delete Project2 directory

--- Project2/pass.js
+++
@@ -1,9 +0,0 @@
-const label = document.querySelector('label')
-const input = document.querySelector('input')
-
-
-if(input.value === ""){
-    console.log(input.value);
-}else{
-    label.style.display = 'none';
-}
